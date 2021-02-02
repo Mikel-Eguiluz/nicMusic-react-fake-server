9f25d68a20d9cba3f89c8837517665b3de9e1cdd
Mikel <gusidam@gmail.com>
2021-02-02 18:01:37 +0000

    adjusted data for my app

--- data.js
+++ data.js
@@ -1,15 +1,36 @@
 const faker = require("faker");
-const { v4:uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-module.exports = function generateGuests() {
-  const guests = [];
+const instruments = [
+  "Guitar",
+  "Oboe",
+  "Piano",
+  "Trumpet",
+  "Violin",
+  "Hapsicord",
+  "Drums",
+  "Tuba",
+  "Violoncello",
+  "Sax",
+  "Voice",
+  "Flute",
+];
+module.exports = function generateScores() {
+  const scores = [];
   for (let id = 0; id < 50; id += 1) {
-    guests.push({
-      id: uuidv4(),
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email(),
+    const instrumentation = [];
+    for (let i = 0; i < 1 + Math.floor(Math.random() * 5); i += 1) {
+      instrumentation.push(instruments[Math.floor(Math.random() * 12)]);
+    }
+    scores.push({
+      _id: uuidv4(),
+      title: faker.lorem.word(),
+      composer: faker.name.firstName() + " " + faker.name.lastName(),
+      style: faker.music.genre(),
+      instrumentation: instrumentation,
+      stock: 1 + Math.floor(Math.random() * 3),
+      owner: faker.name.firstName(),
     });
   }
-  return { guests };
+  return { scores, instruments };
 };
